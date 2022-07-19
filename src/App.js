71f0bf790495843be0f4d3ddbd33066d01a93085
Shubham Meshram <shubham9325@gmail.com>
2022-07-19 16:13:30 +0530

    my portfolio

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Shubham UI Devaloper
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          See my linkedIn Profile
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
